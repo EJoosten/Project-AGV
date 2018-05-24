dcdc5485e7c6efc5b07d1507568754f05bad9ed9
EJoosten <39192070+EJoosten@users.noreply.github.com>
2018-05-24 21:39:33 +0200

    Bluetooth_switchcase
    Added some features to the switch case just because its fun and to test what is possible.

--- Bluetooth_test.ino
+++ Bluetooth_test.ino
@@ -1,27 +1,67 @@
 /*
-* Bluetooh Basic: LED ON OFF - Avishkar
-* Coder - Mayoogh Girish
-* Website - http://bit.do/Avishkar
-* Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
-* This program lets you to control a LED on pin 13 of arduino using a bluetooth module
+  Bluetooh Basic: LED ON OFF - Avishkar
+  Coder - Mayoogh Girish
+  Website - http://bit.do/Avishkar
+  Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
+  This program lets you to control a LED on pin 13 of arduino using a bluetooth module
 */
+
+#include <Servo.h>
+
+Servo exampleServo;
+
 char data = 0;            //Variable for storing received data
+const int buzzer = 10;
 void setup()
 {
-    Serial.begin(9600);   //Sets the baud for serial data transmission                               
-    pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+  Serial.begin(9600);   //Sets the baud for serial data transmission
+  pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+  pinMode(buzzer, OUTPUT);
+  exampleServo.attach(9);
 }
 void loop()
 {
-   if(Serial.available() > 0)      // Send data only when you receive data:
-   {
-      data = Serial.read();        //Read the incoming data & store into data
-      Serial.print(data);          //Print Value inside data in Serial monitor
-      Serial.print("\n");        
-      if(data == '1')              // Checks whether value of data is equal to 1
-         digitalWrite(13, HIGH);   //If value is 1 then LED turns ON
-      else if(data == '0')         //  Checks whether value of data is equal to 0
-         digitalWrite(13, LOW);    //If value is 0 then LED turns OFF
-   }
+  if (Serial.available() > 0)     // Send data only when you receive data:
+  {
+    data = Serial.read();        //Read the incoming data & store into data
+
+    switch (data)
+    {
+      case '0':
+        Serial.println("case: 0");
+        digitalWrite(13, LOW);
+        break;
+
+      case '1':
+        Serial.println("case: 1");
+        digitalWrite(13, HIGH);
+        break;
+        
+      case 's':
+          exampleServo.write(160);             
+          delay(500);                       
+          exampleServo.write(30);              
+          delay(500);    
+          break;
+          
+      case 'b':
+          tone(buzzer, 250);
+          delay(500);
+          tone(buzzer, 500);
+          delay(500);
+          tone(buzzer, 750);
+          delay(500);
+          tone(buzzer, 1000);
+          delay(500);  
+          noTone(buzzer);
+          break;        
+        
+      default:
+        break;
+
+
+    }
+
+  }
 }
 
