754ea8099c34cc321e6407fd23a73315556a4689
EJoosten <39192070+EJoosten@users.noreply.github.com>
2018-05-24 21:09:15 +0200

    Update Bluetooth_switchcase.ino
    Added an extra case with a servo for test.

--- Bluetooth_switchcase.ino
+++ Bluetooth_switchcase.ino
@@ -5,11 +5,17 @@
   Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
   This program lets you to control a LED on pin 13 of arduino using a bluetooth module
 */
+
+#include <Servo.h>
+
+Servo exampleServo;
+
 char data = 0;            //Variable for storing received data
 void setup()
 {
   Serial.begin(9600);   //Sets the baud for serial data transmission
   pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+  exampleServo.attach(9);
 }
 void loop()
 {
@@ -21,20 +27,21 @@ void loop()
     {
       case '0':
         Serial.println("case: 0");
-        data = Serial.read();
-        Serial.print(data);
-        Serial.print("\n");
         digitalWrite(13, LOW);
         break;
 
       case '1':
         Serial.println("case: 1");
-        data = Serial.read();
-        Serial.print(data);
-        Serial.print("\n");
         digitalWrite(13, HIGH);
         break;
-
+        
+      case 's':
+          exampleServo.write(160);              
+          delay(500);                       
+          exampleServo.write(30);              
+          delay(500);    
+          break;
+        
       default:
         break;
 
