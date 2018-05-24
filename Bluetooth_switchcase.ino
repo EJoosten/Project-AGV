7e3e367711ff530992214a32f22195f7537a9118
EJoosten <39192070+EJoosten@users.noreply.github.com>
2018-05-24 11:58:52 +0200

    Bluetooth_switchcase
    Added a switch in the bluetooth code.

---
+++ Bluetooth_switchcase.ino
@@ -0,0 +1,45 @@
+/*
+  Bluetooh Basic: LED ON OFF - Avishkar
+  Coder - Mayoogh Girish
+  Website - http://bit.do/Avishkar
+  Download the App : https://github.com/Mayoogh/Arduino-Bluetooth-Basic
+  This program lets you to control a LED on pin 13 of arduino using a bluetooth module
+*/
+char data = 0;            //Variable for storing received data
+void setup()
+{
+  Serial.begin(9600);   //Sets the baud for serial data transmission
+  pinMode(13, OUTPUT);  //Sets digital pin 13 as output pin
+}
+void loop()
+{
+  if (Serial.available() > 0)     // Send data only when you receive data:
+  {
+    data = Serial.read();        //Read the incoming data & store into data
+
+    switch (data)
+    {
+      case '0':
+        Serial.println("case: 0");
+        data = Serial.read();
+        Serial.print(data);
+        Serial.print("\n");
+        digitalWrite(13, LOW);
+        break;
+
+      case '1':
+        Serial.println("case: 1");
+        data = Serial.read();
+        Serial.print(data);
+        Serial.print("\n");
+        digitalWrite(13, HIGH);
+        break;
+
+      default:
+        break;
+
+
+    }
+
+  }
+}
