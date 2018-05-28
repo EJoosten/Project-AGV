b0ed6fc03f6fde980db3365a70df935326bb134f
EJoosten <39192070+EJoosten@users.noreply.github.com>
2018-05-28 19:59:17 +0200

    Bluetooth support
    Added bluetooth support and did some bug fixing

--- stepper_control.ino
+++ stepper_control.ino
@@ -2,68 +2,73 @@
 
 #define RPM 360
 #define soortStep 1       //full step, half step, quarter step, gebruikt in de for loops
-#define DIRL 
-#define DIRR
-#define STEPL
-#define STEPR
-#define commandoRechtdoor
-#define commandoAchteruit
-#define commandoLinks
-#define commandoRechts
-#define commandoStop
-
+#define DIRL  1           //random waarde voor code check
+#define DIRR 2            //random waarde voor code check
+#define STEPL 3           //random waarde voor code check 
+#define STEPR 4           //random waarde voor code check
+#define num_of_steps 10   //random waarde voor code check
 
 const int stappenPerRondje = 200;  // change this to fit the number of steps per revolution
-int data;
+char data = 0;
 
-Stepper stepperL(stappenPerRondje,DIRL,STEPL );
-Stepper stepperR(stappenPerRondje,DIRR,STEPR );
+Stepper stepperL(stappenPerRondje, DIRL, STEPL );
+Stepper stepperR(stappenPerRondje, DIRR, STEPR );
 
 
 
-void stapVooruit(int steps){     // 1 stap vooruit voor beide motoren
+void forwardStep(int steps) {    // 1 stap vooruit voor beide motoren
   stepperL.step(1);
   stepperR.step(1);
   delay(5);
 }
-void stapAchteruit(int steps){  // 1 stap achteruit voor beide motoren
+void backwardStep(int steps) { // 1 stap achteruit voor beide motoren
   stepperL.step(-1);
   stepperR.step(-1);
   delay(5);
 }
-void stapRechts(int steps){     // 1 stap met de linkermotor, draaien rond het rechterwiel
+void rightStep(int steps) {    // 1 stap met de linkermotor, draaien rond het rechterwiel
   stepperL.step(1);
   delay(5);
 }
-void stapLinks(int steps){      // 1 stap met de rechtermotor, draaien rond het linkerwiel
+void leftStep(int steps) {     // 1 stap met de rechtermotor, draaien rond het linkerwiel
   stepperR.step(1);
   delay(5);
 }
 
 void setup() {
+  Serial.begin(9600);   //Sets the baud for serial data transmission
   stepperL.setSpeed(RPM);
-  stepperR.setSpeed(RPM); 
+  stepperR.setSpeed(RPM);
 }
 
 void loop() {
-  
- switch(data) {
-  case commandoRechtdoor:
-    forwardStep(num_of_steps);
-    break; 
-  case commandoAchteruit:
-    backwardStep(num_of_steps);
-    break;
-  case commandoLinks:
-    leftStep(num_of_steps);
-    break;
-  case commandoRechts:
-    rightStep(num_of_steps);
-    break;
-  default
-}
- 
- 
-}
+  if (Serial.available() > 0)     // Send data only when you receive data:
+  {
+    data = Serial.read();        //Read the incoming data & store into data
 
+    switch (data) {
+      case 'f':
+        forwardStep(num_of_steps);
+        break;
+
+      case 'b':
+        backwardStep(num_of_steps);
+        break;
+
+      case 'l':
+        leftStep(num_of_steps);
+        break;
+
+      case 'r':
+        rightStep(num_of_steps);
+        break;
+
+      default:
+        break;
+
+    }
+  }
+
+
+}
 
